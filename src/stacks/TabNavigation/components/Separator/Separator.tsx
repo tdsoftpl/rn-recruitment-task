a239b45d1781f29e9981c2075aad76ddf34dfd46
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-12-02 15:33:07 +0100

    Created separator.

---
+++ src/stacks/TabNavigation/components/Separator/Separator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export default function Separator() {
+  return <View style={styles.separator} />;
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 24,
+  },
+});
