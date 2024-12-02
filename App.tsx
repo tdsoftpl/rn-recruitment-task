81b8fad4c76b9ea8d32cc103c68d12346ccdbb08
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-12-02 15:20:49 +0100

    Wrapped app with `<AppLayout/>`.

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 
 import {MainStack} from './src/stacks/Main';
+import AppLayout from './src/layout';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <MainStack />
+      <AppLayout>
+        <MainStack />
+      </AppLayout>
     </NavigationContainer>
   );
 }
